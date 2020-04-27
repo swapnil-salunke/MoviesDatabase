4102dc7b29ad567ab7e2cda5cf0365341e2470ee
NilaakashSingh <singhnilaakash@gmail.com>
2020-04-27 20:49:26 +0530

    Initial Commit (#3)
    Co-authored-by: Singh, Nilaakash Uday (US - Mumbai) <nilasingh@deloitte.com>

---
+++ MovieDemoApp/Views/Home/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieDemoApp
+//
+//  Created by Singh, Nilaakash Uday on 27/04/20.
+//  Copyright © 2020 Singh, Nilaakash Uday (US - Mumbai). All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
